0827e70f8879a9c402d70e92823917348a1992c3
Marcos Mendes <marcos.mendes@live.com>
2021-02-01 20:18:13 -0300

    Update mysql-backup.sh

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -14,7 +14,7 @@ DATABASE=#########################
 find $BACKUP_DIR -mindepth 1 -mtime +$DAYS_TO_KEEP -type f -delete
 
 # DUMP
-$MYSQLDUMP --force --opt --single-transaction --user=$MYSQL_USER -p$MYSQL_PASSWORD --database$
+$MYSQLDUMP --force --opt --single-transaction --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $DATABASE > "$BACKUP_DIR/$DATE.sql"
 
 # Push backups to S3
 s3cmd sync -r --delete-removed $BACKUP_DIR/ s3://$S3_BUCKET_NAME
